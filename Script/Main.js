6c0ed96a9b28f59eee051ea8392459e4f310f00e
SINtyanneru <irjitgd@gmail.com>
2025-10-21 07:14:30 +0900

    ページ遷移

--- Script/Main.js
+++ Script/Main.js
@@ -7,6 +7,7 @@ let session = null;
 let self_user = null;
 let current_path = "/";
 let mel = {
+	path_diplay: document.getElementById("PATH_DISPLAY"),
 	dir_contents: document.getElementById("DIR_CONTENTS")
 };
 
@@ -17,12 +18,30 @@ window.addEventListener("load", async (E) => {
 		self_user = await LOGIN(session);
 		if (self_user !== false) {
 			current_path = `/home/${self_user.ID}`;
+			refresh_path_display();
 		} else {
 			window.location.href = LOGIN_PAGE;
 			return;
 		}
 	}
 
+	await reload_dir();
+});
+
+window.addEventListener("popstate", async (e)=>{
+	const path = new URLSearchParams(window.location.search).get("PATH");
+	if (path !== current_path) {
+		current_path = path;
+		refresh_path_display();
+		await reload_dir();
+	}
+});
+
+mel.path_diplay.addEventListener("keydown", async (e)=>{
+	if (e.key !== "Enter") return;
+
+	current_path = mel.path_diplay.value;
+	refresh_path_display();
 	await reload_dir();
 });
 
@@ -33,5 +52,13 @@ async function reload_dir() {
 	for (let i = 0; i < list.length; i++) {
 		const row = list[i];
 		mel.dir_contents.appendChild(await gen_dir_item(row));
+	}
+}
+
+function refresh_path_display() {
+	mel.path_diplay.value = current_path;
+
+	if (decodeURIComponent(window.location.search) != "?PATH=" + current_path) {
+		history.pushState("", "", "?PATH=" + current_path);
 	}
 }
